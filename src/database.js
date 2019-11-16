8d6dc593690101f71a584f921c961ba9da8e1143
Richard Bettridge <r.bettridge@vehikl.com>
2019-11-15 22:36:35 -0500

    new line, fun

--- src/database.js
+++ src/database.js
@@ -4,7 +4,8 @@ const config = require('./json/config.json');
 const sequelize = new Sequelize(
     config.database.name, 
     config.database.user, 
-    config.database.password, {
+    config.database.password, 
+    {
         host: config.database.host,
         dialect: 'mysql',
         operatorsAliases: false,
